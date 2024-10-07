7de5eb60d756d609bfb668806d31959a0f783e76
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-07 21:05:04 +0500

    sms

--- sms_surxandaryo/middlewares/asyncFunctionHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncFunctionHandler = (fn) => {
-    return (...args) => {
-        return Promise.resolve(fn(...args)).catch((error) => {
-            throw new Error(error);
-        });
-    };
-};
-
-module.exports = asyncFunctionHandler;
